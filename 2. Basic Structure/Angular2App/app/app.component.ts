69d221735cd89c2441f31eaf39c363730ac47cbd
Sahil Malik <sahilmalik@winsmarts.com>
2016-08-28 02:42:30 -0400

    Added Angular2App

---
+++ 2. Basic Structure/Angular2App/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: `
+    <h1>My First Angular 2 App</h1>
+    <b>Hello {{name}}</b><br/>
+    <ul>
+        <li *ngFor="#hobby of hobbies;let i = index">
+            {{i}}. {{hobby.id}}: {{hobby.name}}
+        </li>
+    </ul>
+    `
+})
+export class AppComponent {
+    name: string;
+    private hobbies: Hobby[] = [
+        { "id": 1, "name": "Facebook" },
+        { "id": 2, "name": "Reddit" },
+        { "id": 3, "name": "Twitter" }
+    ]
+    constructor() {
+        this.name = "Sahil";
+    }
+}
+
+interface Hobby {
+    id: number,
+    name: string
+}
