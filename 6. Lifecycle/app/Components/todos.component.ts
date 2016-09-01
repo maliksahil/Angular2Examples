ae704190685890758690c6a0b0259d7595e19008
Sahil Malik <sahilmalik@winsmarts.com>
2016-09-01 08:44:11 -0400

    RC5 Lifecycle

---
+++ 6. Lifecycle/app/Components/todos.component.ts
@@ -0,0 +1,18 @@
+import { Component} from '@angular/core';
+
+import {Todo} from '../BusinessObjects'
+import {TodoComponent} from '../Components/todo.component';
+
+@Component({
+    selector: 'todos',
+    templateUrl: './app/Components/todos.component.html',
+    directives: [TodoComponent]
+})
+
+
+export class TodosComponent {
+    private _todos: Array<Todo> = [];
+    constructor() {
+        this._todos.push(new Todo({ "id": 0, "title": "Steal donut truck" }));
+    }
+}
