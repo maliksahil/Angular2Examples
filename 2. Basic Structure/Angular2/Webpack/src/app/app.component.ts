efa083d19f6ff296bb83edae531b1f358aa6c441
Sahil Malik <sahilmalik@winsmarts.com>
2017-02-28 02:02:55 -0500

    Updated angular version, added SystemJS and WebPack examples

---
+++ 2. Basic Structure/Angular2/Webpack/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: `
+    <h1>My First Angular App</h1>
+    <b>Hello {{name}}</b><br/>
+    <ul>
+        <li *ngFor="let hobby of hobbies;let i = index">
+            {{i}}. {{hobby.id}}: {{hobby.name}}
+        </li>
+    </ul>
+    `
+})
+export class AppComponent {
+    name: string;
+    private hobbies: Hobby[] = [
+        { "id": 1, "name": "Facebook" },
+        { "id": 2, "name": "Reddit" },
+        { "id": 3, "name": "Twitter" }
+    ]
+    constructor() {
+        this.name = "Sahil";
+    }
+}
+
+interface Hobby {
+    id: number,
+    name: string
+}
---
+++ 2. Basic Structure/Angular2/Webpack/src/app/main.ts
@@ -0,0 +1,5 @@
+import 'zone.js';
+import 'reflect-metadata';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+platformBrowserDynamic().bootstrapModule(AppModule);
