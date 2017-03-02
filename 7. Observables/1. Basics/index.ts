a255acc6a37c8d889b30aeeb4216b8a623f5ebf2
Sahil Malik <sahilmalik@winsmarts.com>
2017-03-02 01:22:27 -0500

    Updated 7 to Webpack

--- 7. Observables/1. Basics/index.ts
+++ 7. Observables/1. Basics/index.ts
@@ -1,37 +1,26 @@
 import {Observable} from 'rxjs/Rx';
 
-basicObservable();
-mapOperator();
-mapAndFilter();
-reduceOperator();
-scanOperator();
+// basicObservable();
+// mapOperator();
+// mapAndFilter();
+// reduceOperator();
+// scanOperator();
 shareOperator();
 
-// share operator prevents seperate processing chains for each subscriber
-function shareOperator() {
+// cold observable, and is not shared by default - seperate processing chain for each observable.
+function basicObservable() {
     let obs = Observable.interval(500).take(1)
-        .do(i => console.log("value:" + i))
-        .share();
+        .do(i => console.log("value:" + i));
 
     obs.subscribe(value => console.log("observer 1 received:" + value));
     obs.subscribe(value => console.log("observer 2 received:" + value));
 }
 
-// scan operator is like reduce, but doesn't wait until end to return accumulated result. 
-// Instead returns intermediate values as it goes.
-function scanOperator() {
+// map operator allows me to run a method on each value before sending it back.
+function mapOperator() {
     let obs = Observable.interval(500).take(5)
         .do(i => console.log("value:" + i))
-        .scan((state, value) => state + value, 0);
-
-    obs.subscribe(value => console.log("observer 1 received:" + value));
-}
-
-// reduce operator takes all inputs, runs the reducer function on them, and returns the accumulated (final) value
-function reduceOperator() {
-    let obs = Observable.interval(500).take(5)
-        .do(i => console.log("value:" + i))
-        .reduce((state, value) => state + value, 0);
+        .map(i => 2 * i);
 
     obs.subscribe(value => console.log("observer 1 received:" + value));
 }
@@ -47,19 +36,30 @@ function mapAndFilter() {
     obs.subscribe(value => console.log("observer 1 received:" + value));
 }
 
-// map operator allows me to run a method on each value before sending it back.
-function mapOperator() {
+// reduce operator takes all inputs, runs the reducer function on them, and returns the accumulated (final) value
+function reduceOperator() {
     let obs = Observable.interval(500).take(5)
         .do(i => console.log("value:" + i))
-        .map(i => 2 * i);
+        .reduce((state, value) => state + value, 0);
 
     obs.subscribe(value => console.log("observer 1 received:" + value));
 }
 
-// cold observable, and is not shared by default - seperate processing chain for each observable.
-function basicObservable() {
-    let obs = Observable.interval(500).take(1)
-        .do(i => console.log("value:" + i));
+// scan operator is like reduce, but doesn't wait until end to return accumulated result. 
+// Instead returns intermediate values as it goes.
+function scanOperator() {
+    let obs = Observable.interval(500).take(5)
+        .do(i => console.log("value:" + i))
+        .scan((state, value) => state + value, 0);
+
+    obs.subscribe(value => console.log("observer 1 received:" + value));
+}
+
+// share operator prevents seperate processing chains for each subscriber
+function shareOperator() {
+    let obs = Observable.interval(500).take(5)
+        .do(i => console.log("value:" + i))
+        .share();
 
     obs.subscribe(value => console.log("observer 1 received:" + value));
     obs.subscribe(value => console.log("observer 2 received:" + value));
