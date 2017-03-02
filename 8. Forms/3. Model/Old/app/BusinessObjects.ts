076ac4915da36fd86d877a8b4cf368c9d2953d38
Sahil Malik <sahilmalik@winsmarts.com>
2017-03-02 03:18:02 -0500

    Removed stray code in 8.3

--- 8. Forms/3. Model/Old/app/BusinessObjects.ts
+++
@@ -1,9 +0,0 @@
-export class Todo {
-    public id: number;
-    public title: string;
-    public rowState:string;
-    constructor(todo?: Todo) {
-        Object.assign(this, todo);
-        this.rowState = "ADDED";
-    }
-}
--- 8. Forms/3. Model/Old/app/main.ts
+++
@@ -1,5 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
-import { Http } from '@angular/http';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
