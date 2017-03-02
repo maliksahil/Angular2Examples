a9e93dfc201b80355fa8ff7fdd243564fcbfafcd
Sahil Malik <sahilmalik@winsmarts.com>
2017-03-02 16:19:47 -0500

    Updated 9/2 to webpack

---
+++ 9. Routing/2. Passing Parameters/webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    entry: "./src/app/main",
+    output: { filename: "./src/dist/bundle.js"},
+    module: {
+        loaders: [
+            {
+                test: /ts$/,
+                loader: "ts-loader"
+            },
+            {
+                test: /html$/,
+                loader: "html-loader"
+            },
+            {
+                test: /css$/,
+                loader: "css-loader"
+            }                        
+        ]
+    },
+    resolve: {
+        extensions: ["", ".ts", ".js", ".html", ".css"]
+    },
+    devtool: "#inline-source-map"
+}
