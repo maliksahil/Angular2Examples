45e084cb9e318dcbe950a11191cef54de28a61cd
Sahil Malik <sahilmalik@winsmarts.com>
2017-03-27 14:31:39 -0400

    Updated 6 to 4.0.0

--- 6. Services/2. TestDrivenDev/e2e/app.e2e-spec.ts
+++ 6. Services/2. TestDrivenDev/e2e/app.e2e-spec.ts
@@ -1,10 +1,10 @@
-import { TDDPage } from './app.po';
+import { TestDriveDevPage } from './app.po';
 
-describe('tdd App', () => {
-  let page: TDDPage;
+describe('test-drive-dev App', () => {
+  let page: TestDriveDevPage;
 
   beforeEach(() => {
-    page = new TDDPage();
+    page = new TestDriveDevPage();
   });
 
   it('should display message saying app works', () => {
--- 6. Services/2. TestDrivenDev/e2e/app.po.ts
+++ 6. Services/2. TestDrivenDev/e2e/app.po.ts
@@ -1,6 +1,6 @@
 import { browser, element, by } from 'protractor';
 
-export class TDDPage {
+export class TestDriveDevPage {
   navigateTo() {
     return browser.get('/');
   }
